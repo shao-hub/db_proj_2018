5db56e3ea75aa694687dfc753dac854cabe6fd11
stdio2016 <randomfrank8@gmail.com>
2018-05-14 01:02:58 +0800

    show error page

---
+++ application/views/problem/403.php
@@ -0,0 +1,6 @@
+<div class="content">
+<section>
+    <h1>這裡不是你應該來的地方喔</h1>
+    <p>您可以按<a href="<?= URL ?>">這裡</a>，回到首頁</p>
+</section>
+</div>
---
+++ application/views/problem/405.php
@@ -0,0 +1,6 @@
+<div class="content">
+<section>
+    <h1>方法不允許</h1>
+    <p>你正在嘗試駭入我們的網站！</p>
+</section>
+</div>
