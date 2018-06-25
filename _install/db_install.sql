e93a388508f6dc73b82bf3550e8b1a81e0689e84
stdio2016 <randomfrank8@gmail.com>
2018-06-25 14:54:14 +0800

    add event description column and fix dump encoding

--- _install/db_install.sql
+++ _install/db_install.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+-- MySQL dump 10.16  Distrib 10.1.31-MariaDB, for Win32 (AMD64)
 --
 -- Host: localhost    Database: db_proj_2018
 -- ------------------------------------------------------
--- Server version	5.7.22-0ubuntu0.16.04.1
+-- Server version	10.1.31-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,10 +23,10 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `id` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `name` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(127) CHARACTER SET utf8 NOT NULL,
+  `id` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(127) NOT NULL,
   `is_admin` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -38,7 +38,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES ('0400000','$2y$10$0KJKD8.f/fIa7rt/PnnsYO5UskokLwIDTSofOlAZFMib2C/KkKlsO','ZERO','',NULL),('0400001','$2y$10$ABoCGSqgEDMZeom9FEvJb.2xJ3gm30kiyXADBqy1YI/6zuKUlyYuO','ONE','',NULL),('0400002','$2y$10$WyEXzAT8Upzkt2BSlE72fe8z7plJnKPw7rLarIWthpl/iMdChUDZy','äºŒè™Ÿ','',NULL),('777','$2y$10$tmr4IEGQYLSG405g6MpweeNVhDCYFO4ajkhWPfZNi2IRLXXOA7yfW','root','',1);
+INSERT INTO `account` VALUES ('0400000','$2y$10$0KJKD8.f/fIa7rt/PnnsYO5UskokLwIDTSofOlAZFMib2C/KkKlsO','ZERO','',NULL),('0400001','$2y$10$ABoCGSqgEDMZeom9FEvJb.2xJ3gm30kiyXADBqy1YI/6zuKUlyYuO','ONE','',NULL),('0400002','$2y$10$WyEXzAT8Upzkt2BSlE72fe8z7plJnKPw7rLarIWthpl/iMdChUDZy','二號','',NULL),('777','$2y$10$tmr4IEGQYLSG405g6MpweeNVhDCYFO4ajkhWPfZNi2IRLXXOA7yfW','root','',1);
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,9 +51,9 @@ DROP TABLE IF EXISTS `anncs`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `anncs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `title` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `description` varchar(2048) CHARACTER SET utf8 DEFAULT NULL,
+  `description` varchar(2048) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -77,10 +77,11 @@ DROP TABLE IF EXISTS `events`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `team_limit` int(11) NOT NULL,
   `team_size_limit` int(11) NOT NULL,
+  `description` varchar(2048) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -91,7 +92,7 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` VALUES (6,'baseball','2018-05-10',3,20);
+INSERT INTO `events` VALUES (6,'baseball','2018-05-10',3,20,'棒球好好玩');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,7 +105,7 @@ DROP TABLE IF EXISTS `team_members`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `team_members` (
   `team_id` int(11) NOT NULL,
-  `user_id` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `user_id` varchar(45) NOT NULL,
   KEY `fk_team_members_account_idx` (`user_id`),
   KEY `fk_team_members_teams_idx` (`team_id`),
   CONSTRAINT `fk_team_members_account` FOREIGN KEY (`user_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -131,7 +132,7 @@ DROP TABLE IF EXISTS `teams`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `teams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) NOT NULL,
   `event_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_teams_events_idx` (`event_id`),
@@ -145,7 +146,7 @@ CREATE TABLE `teams` (
 
 LOCK TABLES `teams` WRITE;
 /*!40000 ALTER TABLE `teams` DISABLE KEYS */;
-INSERT INTO `teams` VALUES (3,'IAmTeam3',6),(4,'IAmTeam4',6),(5,'Super Team',6),(9,'asdasdasd',6),(11,'ddd',6),(13,'gergerge',6),(14,'testing vvv',6),(17,'asdasdasd',6),(18,'asdasdasd',6),(19,'asdasdasdddd',6),(20,'asdasdas',6);
+INSERT INTO `teams` VALUES (3,'IAmTeam3',6),(9,'asdasdasd',6),(20,'asdasdas',6);
 /*!40000 ALTER TABLE `teams` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -158,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-06 11:29:43
+-- Dump completed on 2018-06-25 14:38:21
