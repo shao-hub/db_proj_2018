d072626693e13b3dea244143b065f1b9d7ab431b
stdio2016 <randomfrank8@gmail.com>
2018-06-26 15:46:09 +0800

    change login/register button

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -3,11 +3,17 @@
     <!-- main content output -->
     <div>
         <form action="<?php echo URL; ?>login/loginAccount" method="POST">
-            <label>ID</label>
-            <input type="text" name="user_id" value="" required><br>
-            <lable>Password</lable>
-            <input type="password" name="user_pw" value="" required><br>
-            <input type="submit" name="submit_login_account" value="登入" />
+            <p>
+            <label>學號</label>
+            <input class="wide" type="text" name="user_id" value="" required>
+            </p>
+            <p>
+            <lable>密碼</lable>
+            <input class="wide" type="password" name="user_pw" value="" required><br>
+            </p>
+            <p>
+            <input class="button cyan" type="submit" name="submit_login_account" value="登入" />
+            </p>
         </form>
     </div>
     <p>沒有帳號？請<a href="<?= URL ?>register">註冊</a></p>
